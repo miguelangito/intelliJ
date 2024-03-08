757a5f25813c44295842a341ab4d3cb228673d61
miguelangito <miguelangelespinalospina@gmail.com>
2024-03-08 13:05:44 -0500

    inicio stock

---
+++ java/POO/src/Stock.java
@@ -0,0 +1,48 @@
+public class Stock {
+    private String descripcion;
+    private int id;
+    private int cantidadStock;
+
+    public Stock(String descripcion, int id, int cantidadStock) {
+        this.descripcion = descripcion;
+        this.id = id;
+        this.cantidadStock = cantidadStock;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public void agregarProducto(String nombre,int cantidad){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "descripcion='" + descripcion + '\'' +
+                ", id=" + id +
+                ", cantidadStock=" + cantidadStock +
+                '}';
+    }
+}
